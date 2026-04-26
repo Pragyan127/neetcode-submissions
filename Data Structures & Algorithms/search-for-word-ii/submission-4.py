771568b5eb048ad85d22768ef994e16ba569969c
Pragyan Dhungana <127623407+Pragyan127@users.noreply.github.com>
2026-04-26 17:37:26 +0545

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,53 @@
+class Solution:
+    def __init__(self):
+        self.dic = {}
+    
+    def addword(self, word):
+        d = self.dic
+        for c in word:
+            if c not in d:
+                d[c] = {}
+            d = d[c]
+        d['.'] = word
+        return d
+
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+
+        for n in words:
+            self.addword(n)
+        res = set()
+        rows = len(board)
+        cols = len(board[0])
+        def dfs(r, c, d):
+
+            if r<0 or r>=rows or c <0 or c>=cols:
+                return
+            
+            ch = board[r][c]
+            if ch not in d:
+                return
+            
+            d = d[ch]
+
+            if '.' in d:
+                res.add(d['.'])
+            
+            board[r][c] = '#'
+
+            dfs(r+1,c,d)
+            dfs(r,c+1,d)
+            dfs(r-1,c,d)
+            dfs(r,c-1,d)
+
+            board[r][c] = ch
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                dfs(r,c,self.dic)
+        
+        return list(res)
+        
+#TC = O(W * L + m * n * 4^L) W = no of words, m*n grids, 4 directions visit
+#SC = O(W * L + L), ≈ O(W * L) L = Len of word
+

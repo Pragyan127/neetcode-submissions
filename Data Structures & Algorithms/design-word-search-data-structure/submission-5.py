9fb46df87586373e26fa56c015e646bc6081f89e
Pragyan Dhungana <127623407+Pragyan127@users.noreply.github.com>
2026-04-26 17:02:07 +0545

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -0,0 +1,34 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.dic = {}
+
+    def addWord(self, word: str) -> None:
+        d = self.dic
+        for  c in word:
+            if c not in d:
+                d[c] = {}
+            d = d[c]
+        d ['.'] = '.'
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(node, i):
+            if len(word) == i:
+                return '.' in node
+            
+            c = word[i]
+
+            if c == '.':
+                for key in node:
+                    if key != '.' and dfs(node[key], i+1):
+                        return True
+                return False
+            else:
+                if c not in node:
+                    return False
+                return dfs(node[c], i+1)
+            
+        return dfs(self.dic, 0)
+
+        

d5637a036e64ffb863bc452f1eb65e1e197892b0
Pragyan Dhungana <127623407+Pragyan127@users.noreply.github.com>
2026-04-30 14:15:40 +0545

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+         return len(nums)*(len(nums)+1)//2 - sum(nums)
+
+        #  TC = O(n) because of sum(nums), SC =O(1)
+
